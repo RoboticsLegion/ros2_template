db500afacca400da47313feae85aa1070500ebe8
amirtronics <amir.h.ebrahimnezhad@gmail.com>
2024-06-03 08:22:44 -0700

    img_subscriber.py: update

--- ros2_template/img_subscriber.py
+++ ros2_template/img_subscriber.py
@@ -1,14 +1,29 @@
+#! /usr/bin/env python
 
 import sys
 import rclpy
 from rclpy.node import Node
 
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+
 
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
-        pass
+        self.publisher_ = self.create_publisher(Image, 'image_raw', 10)
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.publish_image)
+        self.bridge = CvBridge()
 
+
+    def publish_image(self):
+        img = cv2.imread()
+        img_msg = self.bridge.cv2_to_imgmsg(img, encoding="bgr8")
+
+        self.publisher_.publish(img_msg)
+        self.get_logger().info('Image published')
 
     def timer_callback(self):
         pass
